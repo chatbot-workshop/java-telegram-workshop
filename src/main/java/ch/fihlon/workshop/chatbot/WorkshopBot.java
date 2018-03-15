a587e4b454bd46c0ba997a89fed6d9b68da0730c
Marcus Fihlon <marcus@fihlon.ch>
2018-03-15 23:10:31 +0200

    :sparkles: Adding new ability to say hello world

--- src/main/java/ch/fihlon/workshop/chatbot/WorkshopBot.java
+++ src/main/java/ch/fihlon/workshop/chatbot/WorkshopBot.java
@@ -21,9 +21,36 @@ package ch.fihlon.workshop.chatbot;
 import com.google.common.annotations.VisibleForTesting;
 import org.telegram.abilitybots.api.bot.AbilityBot;
 import org.telegram.abilitybots.api.db.DBContext;
+import org.telegram.abilitybots.api.objects.Ability;
+import org.telegram.abilitybots.api.objects.EndUser;
+import org.telegram.abilitybots.api.objects.Flag;
+import org.telegram.abilitybots.api.objects.Reply;
+import org.telegram.abilitybots.api.sender.MessageSender;
+import org.telegram.abilitybots.api.sender.SilentSender;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.GetFile;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.api.objects.PhotoSize;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+
+import static java.util.stream.Collectors.joining;
+import static org.telegram.abilitybots.api.objects.Flag.TEXT;
+import static org.telegram.abilitybots.api.objects.Locality.ALL;
+import static org.telegram.abilitybots.api.objects.Privacy.PUBLIC;
+import static org.telegram.abilitybots.api.util.AbilityUtils.getChatId;
 
 public class WorkshopBot extends AbilityBot {
 
@@ -49,6 +76,17 @@ public class WorkshopBot extends AbilityBot {
     @Override
     public int creatorId() {
         return 318585602;
+    }
+
+    public Ability sayHelloWorld() {
+        return Ability
+                .builder()
+                .name("hello")
+                .info("says hello world")
+                .locality(ALL)
+                .privacy(PUBLIC)
+                .action(context -> silent.send("Hello world", context.chatId()))
+                .build();
     }
 
 }
